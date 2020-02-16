1f6b44a7f40878e8c226aafd29c2f4b3017829e7
rvsbox <rvsbox@gmail.com>
2020-02-16 18:11:24 +0200

    Глава 2. Hello World

---
+++ index.js
@@ -0,0 +1,9 @@
+var http = require('http');
+
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello world!');
+}).listen(3000);
+
+console.log('Сервер запущен на localhost:3000; нажмите Ctrl-C для завершения....');
