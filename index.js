8dd37db3cf8fb6837273a847e63594e2fcc3ff54
rvsbox <rvsbox@gmail.com>
2020-02-16 18:24:23 +0200

    Глава 2. Маршрутизация

--- index.js
+++ index.js
@@ -2,8 +2,24 @@ var http = require('http');
 
 
 http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello world!');
+// Приводим URL к единому виду путем удаления
+// строки запроса, необязательной косой черты
+// в конце строки и приведения к нижнему регистру
+  var path = req.url.replace(/\/?(?:\?.*)?$/, '').toLowerCase();
+  switch (path) {
+    case '':
+      res.writeHead(200, {'Content-Type': 'text/plain'});
+      res.end('Homepage');
+      break;
+    case '/about':
+      res.writeHead(200, {'Content-Type': 'text/plain'});
+      res.end('O');
+      break;
+    default:
+      res.writeHead(404, {'Content-Type': 'text/plain'});
+      res.end('Не найдено');
+      break;
+  }
 }).listen(3000);
 
-console.log('Сервер запущен на localhost:3000; нажмите Ctrl-C для завершения....');
+console.log('Сервер запущен на localhost:3000; нажмите Ctrl+C для завершения....');
