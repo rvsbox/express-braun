7b42141ba06c2f4a74ee949c83f88e37fca8b82e
rvsbox <rvsbox@live.ru>
2020-04-04 00:44:24 +0200

    Глава 3. Шаблонизатор Handlebars

--- index.js
+++ index.js
@@ -1,35 +1,52 @@
-var express = require('express');
+const express = require('express');
+const exphbs = require('express-handlebars');
 
-var app = express();
+const app = express();
+
+// установка механизма представления handlebars
+const hbs = exphbs.create({
+  defaultLayout: 'main',
+  extname: 'hbs'
+})
+app.engine('hbs', exphbs());
+app.set('view engine', 'hbs');
+
 
 app.set('port', process.env.PORT || 3000);
 
+app.use(express.static(__dirname + '/public'));
+
+var fortuneCookies = [
+  "Conquer your fears or they will conquer you.",
+  "Rivers need springs.",
+  "Do not fear what you don't know.",
+  "You will have a pleasant surprise.",
+  "Whenever possible, keep it simple.",
+];
+
 app.get('/', function (req, res) {
-  res.type('text/plain');
-  res.send('Meadowlark Travel');
+  res.render('home');
 });
 
 app.get('/about', function (req, res) {
-  res.type('text/plain');
-  res.send('О Meadowlark Travel');
+  var randomFortune =
+    fortuneCookies[Math.floor(Math.random() * fortuneCookies.length)];
+  res.render('about', { fortune: randomFortune });
 });
 
-// пользовательская страница 404
-app.use(function (req, res) {
-  res.type('text/plain');
+// обобщенный обработчик 404 (промежуточное ПО)
+app.use(function (req, res, next) {
   res.status(404);
-  res.send('404 — Не найдено');
+  res.render('404');
 });
 
-// пользовательская страница 500
+// обработчик ошибки 500 (промежуточное ПО)
 app.use(function (err, req, res, next) {
   console.error(err.stack);
-  res.type('text/plain');
   res.status(500);
-  res.send('500 — Ошибка сервера');
 });
 
 app.listen(app.get('port'), function () {
-  console.log('Express запущен на http://localhost:' +
-    app.get('port') + '; нажмите Ctrl+C для завершения.');
+  console.log('Express started on http://localhost:' +
+    app.get('port') + '; press Ctrl-C to terminate.');
 });
